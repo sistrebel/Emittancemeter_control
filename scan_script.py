39ca9c14b336a73f6cd1746a46da028f39ddad79
sistrebel <127335757+sistrebel@users.noreply.github.com>
2023-11-09 16:07:52 +0100

    changed without readout trigger...

--- scan_script.py
+++ scan_script.py
@@ -109,7 +109,7 @@ def start_scan(motor1,motor2,motor3,number_of_points,x_length,y_length,server): 
     server.issue_motor_command(motor2.command_queue,("calibrate",),isreturn = 0)
     server.issue_motor_command(motor3.command_queue,("calibrate",),isreturn = 0)
     
-    while motor1.iscalibrating == True or motor2.iscalibrating == True or motor3.iscalibrating == True: #wait for calibration to be done
+    while motor1.iscalibrating == True or motor2.iscalibrating == True: #or motor3.iscalibrating == True: #wait for calibration to be done
         time.sleep(0.1)
     #axis length in steps, parameters to adjust for specific situation...
     # x_length = 40000
