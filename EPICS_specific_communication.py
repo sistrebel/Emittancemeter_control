82542a5df628803edefd4b4a24b99cca85b4053f
Strebel Silas <silas.strebel@psi.ch>
2023-11-16 17:12:04 +0100

    ajdusted communications with waiting bool

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -529,20 +529,21 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         #     time.sleep(0.1)
         #     print("calibrating")
         server.issue_motor_command(motor1, ("set_speed",1300))
+        
         server.issue_motor_command(motor2, ("set_speed",1300))
         #print("here")
-        time.sleep(0.1)
+        
         
         server.issue_motor_command(motor1, ("go_to_position",1000)) #do not return from this;((()))
-        server.issue_motor_command(motor2, ("set_speed",1300))
+        server.issue_motor_command(motor2, ("go_to_position",1000))
         server.issue_motor_command(motor1, ("go_to_position",0))
-        server.issue_motor_command(motor2, ("set_speed",1300))
+        server.issue_motor_command(motor2, ("go_to_position",0))
         
-        server.issue_motor_command(motor1, ("go_to_position",2000))
+        #server.issue_motor_command(motor1, ("go_to_position",2000))
         
         
         server.issue_motor_command(motor1, ("calibrate",))
-        server.issue_motor_command(motor2, ("set_speed",1300))
+        server.issue_motor_command(motor2, ("calibrate",))
         
         #server.issue_motor_command(command_queue, ("move_left", 5000))
         #server.issue_motor_command(command_queue, ("move_right",20000))
