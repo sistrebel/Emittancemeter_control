a1502b11b3c7b0b35b9dc0190bc5206437d921ad
Strebel Silas <silas.strebel@psi.ch>
2023-11-17 17:13:56 +0100

    adjusted dist and ramp

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -458,7 +458,7 @@ class MotorClient(): #i don't know if Thread is necessary
         if self.Get(self.pv_speed_get) == None:
             return
         
-        self.pv_command.put(1) #enumerated calCCW to 1 i think 
+        self.pv_command.put(6) #enumerated calCCW to 1 i think , 6 is calCCW2
             
         self.iscalibrating = True
         #time.sleep(0.2)
