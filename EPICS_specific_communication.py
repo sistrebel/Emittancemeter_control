94ac1db2d685a8d1fad3af977a4b873bf8c71f4d
Strebel Silas <silas.strebel@psi.ch>
2023-11-17 12:11:00 +0100

    adjusted dist and ramp

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -595,17 +595,18 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         server.issue_motor_command(motor2, ("go_to_position",0))
         #server.issue_motor_command(motor2, ("calibrate",))
         #server.issue_motor_command(motor1, ("calibrate",))
-        server.issue_motor_command(motor1, ("go_to_position",2000))
-        server.issue_motor_command(motor2, ("go_to_position",2000))
+        server.issue_motor_command(motor1, ("go_to_position",1100))
+        server.issue_motor_command(motor2, ("go_to_position",1100))
         
         
-        server.issue_motor_command(motor2, ("go_to_position",0))
+        #
+        server.issue_motor_command(motor2, ("go_to_position",0))#this command is lost
         
         server.issue_motor_command(motor1, ("go_to_position",0))
         
-        server.issue_motor_command(motor1, ("go_to_position",100))
+        # server.issue_motor_command(motor1, ("go_to_position",100))
         
-        server.issue_motor_command(motor2, ("go_to_position",100))
+        # server.issue_motor_command(motor2, ("go_to_position",100))
         
         
         
