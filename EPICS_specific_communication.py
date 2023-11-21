0737002842c4611e7f3146e86d97e5d0eea31928
Strebel Silas <silas.strebel@psi.ch>
2023-11-21 12:19:10 +0100

    adjusted settings for third motor

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -629,8 +629,8 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
     #try:
         
         # Example: Move motor 1 by 1000 steps
-        server.issue_motor_command(motor2, ("calibrate",))
-        server.issue_motor_command(motor1, ("calibrate",))
+        # server.issue_motor_command(motor2, ("calibrate",))
+        # server.issue_motor_command(motor1, ("calibrate",))
         #server.issue_motor_command(motor3, ("calibrate",))
         # time.sleep(0.1)
         # while motor3.iscalibrating == True: #or motor3.iscalibrating == True: #wait for calibration to be done
@@ -646,10 +646,10 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         
         # server.issue_motor_command(motor3, ("go_to_position",2000)) #do not return from this;((()))
         
-        # server.issue_motor_command(motor2, ("go_to_position",2000))
-        # #time.sleep(0.1)
-        # server.issue_motor_command(motor1, ("go_to_position",2000))
+        server.issue_motor_command(motor2, ("go_to_position",2000))
         #time.sleep(0.1)
+        server.issue_motor_command(motor1, ("go_to_position",2000))
+        time.sleep(0.1)
               
         # server.issue_motor_command(motor3, ("go_to_position",400))
         # server.issue_motor_command(motor1, ("go_to_position",200))
