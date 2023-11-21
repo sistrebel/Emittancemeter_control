5af01edc41041d3bf03f541f5c530273f7830a7d
Strebel Silas <silas.strebel@psi.ch>
2023-11-21 14:39:05 +0100

    adjusted settings for third motor

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -660,9 +660,9 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         
         # server.issue_motor_command(motor3, ("go_to_position",2000)) #do not return from this;((()))
         
-        server.issue_motor_command(motor2, ("go_to_position",500))
-        #time.sleep(0.1)
         server.issue_motor_command(motor1, ("go_to_position",500))
+        #time.sleep(0.1)
+        server.issue_motor_command(motor2, ("go_to_position",500))
         #time.sleep(0.1)
               
         # server.issue_motor_command(motor3, ("go_to_position",400))
