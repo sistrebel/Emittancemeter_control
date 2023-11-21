67ab931b56f6b506c6aaae65340592ac89e6610e
Strebel Silas <silas.strebel@psi.ch>
2023-11-21 16:54:55 +0100

    ajdusted GUI, moving motor only

--- scan_script.py
+++ scan_script.py
@@ -159,9 +159,9 @@ def start_scan(motor1,motor2,motor3,number_of_points,x_length,y_length,server): 
             moving = False
             while moving == False: #wait till motors are free and stopped
                 if motor1.ismoving == False and  motor2.ismoving == False:  #check that motors are actually free to move
-                    server.issue_motor_command(motor1,("go_to_position",point_x),isreturn = 0)  #moves motor on thread one
-                    time.sleep(0.2) #safety
-                    server.issue_motor_command(motor2,("go_to_position",point_y),isreturn = 0) #moves motor on thread two
+                    server.issue_motor_command(motor1,("go_to_position",point_x))  #moves motor on thread one
+                    #time.sleep(0.2) #safety
+                    #server.issue_motor_command(motor2,("go_to_position",point_y)) #moves motor on thread two
                     time.sleep(0.2)
                     moving = True
                     print("starts to move")
