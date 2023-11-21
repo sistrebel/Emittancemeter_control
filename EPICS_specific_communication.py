d23350503f37d3d25f5529729c6bdec3ded49e75
Strebel Silas <silas.strebel@psi.ch>
2023-11-21 15:16:43 +0100

    adjusted settings for third motor

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -621,7 +621,7 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         
         motor1 = server.create_and_start_motor_client(server, 1, command_queue)
         
-        #motor2 = server.create_and_start_motor_client(server, 2, command_queue2)
+        motor2 = server.create_and_start_motor_client(server, 2, command_queue2)
         
         #motor3 = server.create_and_start_motor_client(server, 3, command_queue3)
         
@@ -645,7 +645,7 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         #motor3.calibration()
         # Example: Move motor 1 by 1000 steps
         server.issue_motor_command(motor1, ("calibrate",))
-       # server.issue_motor_command(motor3, ("calibrate",))
+        server.issue_motor_command(motor2, ("calibrate",))
         #time.sleep(5)
         #server.issue_motor_command(motor3, ("calibrate",))
         # time.sleep(0.1)
@@ -664,13 +664,13 @@ if __name__ == "__main__": #is only excecuted if the program is started by itsel
         
         server.issue_motor_command(motor1, ("go_to_position",500))
         #time.sleep(0.1)
-        #server.issue_motor_command(motor3, ("go_to_position",500))
+        server.issue_motor_command(motor2, ("go_to_position",500))
         #time.sleep(0.1)
               
         # server.issue_motor_command(motor3, ("go_to_position",400))
         server.issue_motor_command(motor1, ("go_to_position",200))
         # time.sleep(0.1)
-       # server.issue_motor_command(motor3, ("go_to_position",1000))
+        server.issue_motor_command(motor2, ("go_to_position",200))
         #
         
         #server.issue_motor_command(motor3, ("calibrate",))
