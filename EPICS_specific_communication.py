e1ee387f08ac8648dc7cff0a4687fd33cffa344b
Strebel Silas <silas.strebel@psi.ch>
2023-11-21 11:25:17 +0100

    adjusted settings for third motor

--- EPICS_specific_communication.py
+++ EPICS_specific_communication.py
@@ -499,7 +499,7 @@ class MotorClient(): #i don't know if Thread is necessary
         
         while  status != 0x9 and status != 0xD: #self.Get(self.pv_motor_status) != 0xD and self.Get(self.pv_motor_status) != 0x9 : #didn't reach endstop ye
              #print(self.Get(self.pv_endstopstatus))
-             print("here calibrating")
+             print(status)
              status = self.Get(self.pv_motor_status)
              time.sleep(0.1)
         self.iscalibrating = False
