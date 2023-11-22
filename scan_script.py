439c8d34150dfca4c3825d90409b14bbb448bc3c
Strebel Silas <silas.strebel@psi.ch>
2023-11-22 11:58:57 +0100

    ajdusted GUI, moving motor only

--- scan_script.py
+++ scan_script.py
@@ -183,7 +183,7 @@ def start_scan(motor1,motor2,motor3,number_of_points,x_length,y_length,server): 
                 status3 = motor3.Get(motor3.pv_motor_status)
                 if status1 == 0x9 or status1 == 0x8 or status1 == 0xA or status1 == 0x1 or status1 == 0x0 and motor1.Get(server.pv_status) != 1  : #not moving
                   if status2 == 0x9 or status2 == 0x8 or status2 == 0xA or status2 == 0x1 or status2 == 0x0 and motor2.Get(server.pv_status) != 1 : #not moving
-                      if status3 == 0x9 or motor2.Get(motor2.pv_SOLRB) == endposition_z  and motor3.Get(server.pv_status) != 1 :  #not moving and at upper endpoint
+                      if status3 == 0x9 or motor3.Get(motor3.pv_SOLRB) == endposition_z  and motor3.Get(server.pv_status) != 1 :  #not moving and at upper endpoint
                       
                 # if motor1.ismoving == False and  motor2.ismoving == False:  #check that motors are actually free to move
                         server.issue_motor_command(motor1,("go_to_position",point_x))  #moves motor on thread one
