2b59e716e233a48af03bd7fa3f468e8a4537438b
Strebel Silas <silas.strebel@psi.ch>
2023-11-24 11:24:34 +0100

    new GUI buttons

--- GUI_with_scan.py
+++ GUI_with_scan.py
@@ -404,9 +404,9 @@ class MainWindow(QMainWindow):
     def calibration(self):
         """starts calibration for all three motors"""
         
-        self.server.issue_motor_command(self.motor1,("calibrate"))
-        self.server.issue_motor_command(self.motor2,("calibrate"))
-        self.server.issue_motor_command(self.motor3,("calibrate"))
+        self.server.issue_motor_command(self.motor1,("calibrate",))
+        self.server.issue_motor_command(self.motor2,("calibrate",))
+        self.server.issue_motor_command(self.motor3,("calibrate",))
         # self.motor2.calibration()
         # self.motor3.calibration()
         
