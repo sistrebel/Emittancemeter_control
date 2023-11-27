61421ef3ab9c5f0c6e3a8627a4b0897425d6ef54
Strebel Silas <silas.strebel@psi.ch>
2023-11-27 16:43:05 +0100

    new GUI buttons connected

--- scan_script.py
+++ scan_script.py
@@ -411,8 +411,8 @@ def start_readout(goinsteps,show_message,motor1,motor2,motor3,z_length,meshsize_
               
                 #if status3 == 0x9 or status3 == 0x8 or status3 == 0xA or status3 == 0x1 or status3 == 0x0 and motor3.Get(server.pv_status) != 1: #  and motor3.Get(motor3.pv_SOLRB) == start_point:
                 if command3stat == 0x100 or command3stat == 0x0 :
-                      
                         server.issue_motor_command(motor3,("go_to_position",current_position))
+                        print("now")
                         moving = True
                 else: pass 
    
