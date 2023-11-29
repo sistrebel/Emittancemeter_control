22e5be5f50aa5ecf8fc596f656bfdfe4062096c3
Strebel Silas <silas.strebel@psi.ch>
2023-11-29 11:32:32 +0100

    new plot for current distribution

--- scan_script.py
+++ scan_script.py
@@ -159,7 +159,7 @@ def start_scan(saveit,meas_freq,goinsteps, show_message,show_scan_time,motor1,mo
     
     if meshsize_x > x_length or meshsize_y > y_length or meshsize_z > z_length:
         print(">> INVALID mesh or dimensions")
-        show_message(">> INVALID mesh or dimensions")
+        #show_message(">> INVALID mesh or dimensions")
         return
     
     
