a1fd4829a032a4d5833bbcf561561455af4180a1
Strebel Silas <silas.strebel@psi.ch>
2023-11-29 09:42:46 +0100

    new plot for current distribution

--- GUI_with_scan.py
+++ GUI_with_scan.py
@@ -111,7 +111,7 @@ class MainWindow(QMainWindow):
         #initialize the update timer for meas plot
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_plot_meas)
-        self.timer.start(1000) #updates every 100ms
+        self.timer.start(1000) #updates every 1000ms
         
         #initialize the update timer for the position plot
         self.timer = QTimer(self)
@@ -428,7 +428,7 @@ class MainWindow(QMainWindow):
         goinsteps = False
         saveit = True
         
-        meas_freq = int(self.textEdit_MeasFreq.toPlainText()) #get measurement frequency
+        meas_freq = float(self.textEdit_MeasFreq.toPlainText()) #get measurement frequency
         
         if resolution_x > 0 and resolution_y > 0 and resolution_z > 0:
             scan_thread = threading.Thread(target=scan_script.start_scan, args=(saveit,meas_freq,goinsteps,self.show_message,self.show_scan_time,self.motor1,self.motor2,self.motor3,meshsize_x,meshsize_y,meshsize_z,x1_setup_val,y1_setup_val,y2_setup_val, self.server))
