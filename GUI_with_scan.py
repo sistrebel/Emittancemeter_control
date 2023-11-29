06a4be92bc02810c56a52a34a6471067eb9aed99
Strebel Silas <silas.strebel@psi.ch>
2023-11-29 16:59:20 +0100

    new plot for current distribution

--- GUI_with_scan.py
+++ GUI_with_scan.py
@@ -448,6 +448,12 @@ class MainWindow(QMainWindow):
         
         meas_freq = float(self.textEdit_MeasFreq.toPlainText()) #get measurement frequency
         
+        y2_speed = self.steps_to_mm(y2_setup_val[2],"2Y")
+        
+        fidelity = meas_freq/y2_speed #how many meas points per mm
+        
+        self.textBrowser_Fidelity.append(fidelity)#maybe change this... to another format instead of using append()
+        
         if resolution_x > 0 and resolution_y > 0 and resolution_z > 0:
             
             scan_thread = threading.Thread(target=scan_script.start_scan, args=(saveit,meas_freq,goinsteps,self.message_queue,self.show_scan_time,self.motor1,self.motor2,self.motor3,meshsize_x,meshsize_y,meshsize_z,x1_setup_val,y1_setup_val,y2_setup_val, self.server))
