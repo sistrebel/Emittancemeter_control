ed5dcd7c23374ceb2ccd75cd36691fd22d36d417
Strebel Silas <silas.strebel@psi.ch>
2023-11-30 10:31:17 +0100

    new plot for current distribution

--- GUI_with_scan.py
+++ GUI_with_scan.py
@@ -345,6 +345,7 @@ class MainWindow(QMainWindow):
     def run_show_time_thread(self):  
         """constantly checks the message_queue and passes the messages to the messagebox"""
         self.done_showing = False
+        time.sleep(1)
         while True and self.server.running and self.done_showing == False:
                 # if not self.server.running:
                 #     break
@@ -494,7 +495,7 @@ class MainWindow(QMainWindow):
         else: self.textBrowser_Fidelity.clear()
         
         if resolution_x > 0 and resolution_y > 0 and resolution_z > 0:
-            self.start_show_time_thread()
+            self.start_show_scan_time_thread()
             
             scan_thread = threading.Thread(target=scan_script.start_scan, args=(saveit,meas_freq,goinsteps,self.message_queue,self.show_scan_time_queue,self.motor1,self.motor2,self.motor3,meshsize_x,meshsize_y,meshsize_z,x1_setup_val,y1_setup_val,y2_setup_val, self.server))
             scan_thread.daemon = True
